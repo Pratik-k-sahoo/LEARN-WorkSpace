019adf4fab87d10492ef5ae6c060697c38e19529
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2023-09-03 15:57:18 +0530

    Exercise

--- WebDev-BASICS/JS-Basics/Exercises/Q3.js
+++ WebDev-BASICS/JS-Basics/Exercises/Q3.js
@@ -1,12 +1,12 @@
 let ele = document.querySelectorAll(".box")
-let jokes = [`You know why you never see elephants hiding up in trees?
-Because they’re really good at it.`, `What is red and smells like blue paint?
-Red paint`, `What do you call a parade of rabbits hopping backwards?
-A receding hare-line.`, ` Where does the General keep his armies?
-In his sleevies!`, `Why aren’t koalas actual bears?
-The don’t meet the koalafications.`, `A bear walks into a restaurant and says, “I want a grilled… cheese.” The waiter says “Why the big pause?”
-The bear replies, “I don’t know. I was born with them`, `What do you call bears with no ears?
-B.`, `Why did the tomato blush?
+let jokes = [`You know why you never see elephants hiding up in trees?<br>
+Because they’re really good at it.`, `What is red and smells like blue paint?<br>
+Red paint`, `What do you call a parade of rabbits hopping backwards?<br>
+A receding hare-line.`, ` Where does the General keep his armies?<br>
+In his sleevies!`, `Why aren’t koalas actual bears?<br>
+The don’t meet the koalafications.`, `A bear walks into a restaurant and says, “I want a grilled… cheese.” The waiter says “Why the big pause?”<br>
+The bear replies, “I don’t know. I was born with them`, `What do you call bears with no ears?<br>
+B.`, `Why did the tomato blush?<br>
 Because it saw the salad dressing`, `I went in to a pet shop. I said, “Can I buy a goldfish?” The guy said, “Do you want an aquarium?”
 I said, “I don’t care what star sign it is.`]
 console.log(ele)
