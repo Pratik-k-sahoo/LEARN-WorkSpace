02cc61f2878b725e4398cd2f9dd5ec657a6ccc09
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2023-09-07 01:23:31 +0530

    Culture and Heritage

---
+++ WebDevPROJECT/Cultural and Heritage Site/script/script.js
@@ -0,0 +1,7 @@
+const typed=new Typed('.multiple_text',{
+    strings: ["Incredible India", "Dekho Apna Desh!", " Explore the Indian Culture and Heritage."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
