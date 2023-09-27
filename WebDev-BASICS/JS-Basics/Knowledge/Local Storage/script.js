45cd887d21606cd3fcee455a0f898ac909c52d17
PRATIK KUMAR SAHOO <sahoopratik5@gmail.com>
2023-09-27 23:24:31 +0530

    Local Storage

---
+++ WebDev-BASICS/JS-Basics/Knowledge/Local Storage/script.js
@@ -0,0 +1,6 @@
+localStorage.setItem("name", "pratik")
+let key = prompt("Enter the key you want to set")
+let value = prompt("Enter the value you want to set")
+localStorage.setItem(key, value)
+
+console.log(`The value at ${key} is ${localStorage.getItem(key)}`);
